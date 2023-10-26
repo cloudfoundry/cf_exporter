5f6e0e4753b648fece02ba2ec8ac62208a3cd1df
BG <bguttmann@anynines.com>
2023-10-26 15:47:44 +0200

    Remove dot imports from tests

--- filters/filters_test.go
+++ filters/filters_test.go
@@ -1,87 +1,87 @@
 package filters_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
 
 	"github.com/bosh-prometheus/cf_exporter/filters"
 )
 
-var _ = Describe("Filters", func() {
+var _ = ginkgo.Describe("Filters", func() {
 
-	Describe("Constructing Filters", func() {
+	ginkgo.Describe("Constructing Filters", func() {
 		var (
 			err error
 			f   *filters.Filter
 		)
 
-		Context("with no active filters", func() {
-			BeforeEach(func() {
+		ginkgo.Context("with no active filters", func() {
+			ginkgo.BeforeEach(func() {
 				f, err = filters.NewFilter()
 			})
-			It("no error occurs", func() {
-				Expect(err).To(BeNil())
+			ginkgo.It("no error occurs", func() {
+				gomega.Expect(err).To(gomega.BeNil())
 			})
-			It("all but events are active", func() {
-				Expect(f.Enabled(filters.Applications)).To(BeTrue())
-				Expect(f.Enabled(filters.Buildpacks)).To(BeTrue())
-				Expect(f.Enabled(filters.IsolationSegments)).To(BeTrue())
-				Expect(f.Enabled(filters.Organizations)).To(BeTrue())
-				Expect(f.Enabled(filters.Routes)).To(BeTrue())
-				Expect(f.Enabled(filters.SecurityGroups)).To(BeTrue())
-				Expect(f.Enabled(filters.ServiceBindings)).To(BeTrue())
-				Expect(f.Enabled(filters.ServicePlans)).To(BeTrue())
-				Expect(f.Enabled(filters.Services)).To(BeTrue())
-				Expect(f.Enabled(filters.Spaces)).To(BeTrue())
-				Expect(f.Enabled(filters.Stacks)).To(BeTrue())
-				Expect(f.Enabled(filters.Tasks)).To(BeFalse())
-				Expect(f.Enabled(filters.Events)).To(BeFalse())
+			ginkgo.It("all but events are active", func() {
+				gomega.Expect(f.Enabled(filters.Applications)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Buildpacks)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.IsolationSegments)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Organizations)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Routes)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.SecurityGroups)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.ServiceBindings)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.ServicePlans)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Services)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Spaces)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Stacks)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Tasks)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Events)).To(gomega.BeFalse())
 			})
 		})
 
-		Context("with valid active filters", func() {
-			BeforeEach(func() {
+		ginkgo.Context("with valid active filters", func() {
+			ginkgo.BeforeEach(func() {
 				f, err = filters.NewFilter(filters.Applications, filters.Stacks)
 			})
-			It("no error occurs", func() {
-				Expect(err).To(BeNil())
+			ginkgo.It("no error occurs", func() {
+				gomega.Expect(err).To(gomega.BeNil())
 			})
-			It("only given filters are active", func() {
-				Expect(f.Enabled(filters.Applications)).To(BeTrue())
-				Expect(f.Enabled(filters.Buildpacks)).To(BeFalse())
-				Expect(f.Enabled(filters.IsolationSegments)).To(BeFalse())
-				Expect(f.Enabled(filters.Organizations)).To(BeFalse())
-				Expect(f.Enabled(filters.Routes)).To(BeFalse())
-				Expect(f.Enabled(filters.SecurityGroups)).To(BeFalse())
-				Expect(f.Enabled(filters.ServiceBindings)).To(BeFalse())
-				Expect(f.Enabled(filters.ServicePlans)).To(BeFalse())
-				Expect(f.Enabled(filters.Services)).To(BeFalse())
-				Expect(f.Enabled(filters.Spaces)).To(BeFalse())
-				Expect(f.Enabled(filters.Stacks)).To(BeTrue())
-				Expect(f.Enabled(filters.Tasks)).To(BeFalse())
-				Expect(f.Enabled(filters.Events)).To(BeFalse())
-			})
-
-			It("querying all", func() {
-				Expect(f.All(filters.Applications, filters.Stacks)).To(BeTrue())
-				Expect(f.All(filters.Applications, filters.Spaces)).To(BeFalse())
-				Expect(f.All()).To(BeTrue())
+			ginkgo.It("only given filters are active", func() {
+				gomega.Expect(f.Enabled(filters.Applications)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Buildpacks)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.IsolationSegments)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Organizations)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Routes)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.SecurityGroups)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.ServiceBindings)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.ServicePlans)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Services)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Spaces)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Stacks)).To(gomega.BeTrue())
+				gomega.Expect(f.Enabled(filters.Tasks)).To(gomega.BeFalse())
+				gomega.Expect(f.Enabled(filters.Events)).To(gomega.BeFalse())
 			})
 
-			It("querying any", func() {
-				Expect(f.Any(filters.Applications, filters.Stacks)).To(BeTrue())
-				Expect(f.Any(filters.Applications, filters.Spaces)).To(BeTrue())
-				Expect(f.Any(filters.Organizations, filters.Spaces)).To(BeFalse())
-				Expect(f.Any()).To(BeFalse())
+			ginkgo.It("querying all", func() {
+				gomega.Expect(f.All(filters.Applications, filters.Stacks)).To(gomega.BeTrue())
+				gomega.Expect(f.All(filters.Applications, filters.Spaces)).To(gomega.BeFalse())
+				gomega.Expect(f.All()).To(gomega.BeTrue())
+			})
+
+			ginkgo.It("querying any", func() {
+				gomega.Expect(f.Any(filters.Applications, filters.Stacks)).To(gomega.BeTrue())
+				gomega.Expect(f.Any(filters.Applications, filters.Spaces)).To(gomega.BeTrue())
+				gomega.Expect(f.Any(filters.Organizations, filters.Spaces)).To(gomega.BeFalse())
+				gomega.Expect(f.Any()).To(gomega.BeFalse())
 			})
 		})
 
-		Context("with invalid filters", func() {
-			BeforeEach(func() {
+		ginkgo.Context("with invalid filters", func() {
+			ginkgo.BeforeEach(func() {
 				f, err = filters.NewFilter("I don't exist")
 			})
-			It("an error occurs", func() {
-				Expect(err).NotTo(BeNil())
+			ginkgo.It("an error occurs", func() {
+				gomega.Expect(err).NotTo(gomega.BeNil())
 			})
 		})
 
