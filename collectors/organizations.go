7cc6b61858c77afbc0cd516ca591b7f6cec7abb6
Benjamin Guttmann <bguttmann@anynines.com>
2024-01-09 16:01:43 +0100

    Revert "add field suspended for organizations"
    This reverts commit 74f2b5b074a17229c664677d996c6f1252eb7f76.

--- collectors/organizations.go
+++ collectors/organizations.go
@@ -2,7 +2,6 @@ package collectors
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
 	"code.cloudfoundry.org/cli/resources"
@@ -46,7 +45,7 @@ func NewOrganizationsCollector(
 			Help:        "Labeled Cloud Foundry Organization information with a constant '1' value.",
 			ConstLabels: prometheus.Labels{"environment": environment, "deployment": deployment},
 		},
-		[]string{"organization_id", "organization_name", "quota_name", "suspended"},
+		[]string{"organization_id", "organization_name", "quota_name"},
 	)
 
 	organizationNonBasicServicesAllowedMetric := prometheus.NewGaugeVec(
@@ -328,7 +327,6 @@ func (c OrganizationsCollector) reportOrg(org resources.Organization, objs *mode
 		org.GUID,
 		org.Name,
 		quotaName,
-		strconv.FormatBool(org.Suspended),
 	).Set(float64(1))
 
 	return nil
